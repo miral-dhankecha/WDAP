cb64ffe01e41d84f827b2b1758e9b70cdc1bc54b
Miral <miral.dhankecha127111@marwadiuniversity.ac.in>
2026-06-10 12:39:31 +0530

    Add files via upload

---
+++ Prog1.php
@@ -0,0 +1,41 @@
+<?php
+
+    const COLLEGE_NAME = "Marwadi University";
+    const SEMESTER = "Semester 4";
+
+    $studentName = "Miral Dhankecha";
+    $entno = "92400527161";
+
+    $sub1 = "DM";
+    $grade1 = "S";
+
+    $sub2 = "OS";
+    $grade2 = "B";
+
+    $sub3 = "CN";
+    $grade3 = "A";
+
+    $sub4 = "CSHARP";
+    $grade4 = "A+";
+
+    $sub5 = "PYTHON";
+    $grade5 = "C";
+
+    $sub6 = "LS2";
+    $grade6 = "O";
+
+    echo "<h2>Previous Semester Result</h2>";
+
+    echo "College Name : ".COLLEGE_NAME. "<br>";
+    echo "Semester : ".SEMESTER. "<br>";
+    echo "Student Name : ".$studentName. "<br>";
+    echo "Enrollment No : ".$entno. "<br><br>";
+
+    echo "Subject : " .$sub1. " | Grade : " .$grade1. "<br>";
+    echo "Subject : " .$sub2. " | Grade : " .$grade2. "<br>";
+    echo "Subject : " .$sub3. " | Grade : " .$grade3. "<br>";
+    echo "Subject : " .$sub4. " | Grade : " .$grade4. "<br>";
+    echo "Subject : " .$sub5. " | Grade : " .$grade5. "<br>";
+    echo "Subject : " .$sub6. " | Grade : " .$grade6. "<br>";
+    
+?>
